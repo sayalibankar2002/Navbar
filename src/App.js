c3dc3c604b6ad1ef0e31862e8c9f4ab7807c72d1
Sayali Bankar <84892351+sayalibankar2002@users.noreply.github.com>
2024-07-01 10:21:17 +0530

    Add files via upload
    navbar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./Component/navbar";
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navbar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
